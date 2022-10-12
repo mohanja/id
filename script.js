c8bea124de27b20444744f441076b63845435c71
Mohan Ram <112931580+mohanja@users.noreply.github.com>
2022-10-12 12:49:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo(){
+    var res=document.getElementById("fistname").value;
+var res1=document.getElementById("middelname").value;
+var res2=document.getElementById("lastname").value;
+var res3=document.getElementById("email").value;
+var res4=document.getElementById("password").value;
+
+     return console.log(res,res1,res2,res3,res4)
+}
